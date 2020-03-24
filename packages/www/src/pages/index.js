32c8029a77087117184c5d4f1f502bb55d679b4f
HAINES-DESKTOP\richa <richardkhaines@hotmail.co.uk>
2020-03-24 10:24:11 +0100

    added some theme-ui stuff

--- packages/www/src/pages/index.js
+++ packages/www/src/pages/index.js
@@ -1,7 +1,16 @@
 import React from 'react';
+import {Container, Heading, Button, Flex} from 'theme-ui'
 
 export default () => (
-    <div>
-        <h1>Our Site</h1>
-    </div>
+    <Container>
+        <Flex sx={{ flexDirection: 'column', padding: 3}}>
+        <Heading as="h1">Our Site</Heading>
+        <Button 
+        sx={{marginTop: 2}}
+        onClick={() => {
+            alert('clicked')
+        }}
+        >Login</Button>
+        </Flex>
+    </Container>
 )
